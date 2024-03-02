8bca12c9c7d99fe0a3ba8e53f1e659e048414a66
aleenasiddique <aleenasiddique3@gmail.com>
2024-03-02 23:11:17 +0500

    fetchAI function

--- netlify/functions/fetchAI/fetchAI.js
+++ netlify/functions/fetchAI/fetchAI.js
@@ -1,5 +1,5 @@
 import OpenAI from "openai"
-const { getRestrictedFoods } = require('../../../src/components/Tools')
+const { getRestrictedFoods } = require('../../../../../src/components/Tools')
 
 
 const openai = new OpenAI({
